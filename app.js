bea36319d721eb82052c7e0cca4c11f9f895f837
Julian <julian@studio42.dk>
2016-04-06 00:19:32 +0200

    twitter

--- app.js
+++ app.js
@@ -3,31 +3,37 @@ var express = require('express');
 
 var app = express();
 
-console.log('is twitter stuff? ', (!!process.env.TWITTER_CONSUMER_KEY && !!process.env.TWITTER_CONSUMER_SECRET && !!process.env.TWITTER_BEARER_TOKEN) );
-
 var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    bearer_token: process.env.TWITTER_BEARER_TOKEN
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
+var tweetsCache = null;
+setInterval(function () {
+    tweetsCache = null;
+}, 60000);
+
 app.get('/twitter', function (req, res) {
-    client.get('search/tweets', {q: '#ngj16'}, function(error, tweets, response){
-        if (error) {
-            console.log(error);
-            res.status(500).json({
-                error: error
-            });
-        }
-        else {
-            console.log(tweets);
-            res.json({
-                tweets: tweets
-            });
-        }
-    });
+    if (tweetsCache) {
+        res.json(tweetsCache);
+    }
+    else {
+        client.get('search/tweets', {q: '#ngj16'}, function(error, tweets, response){
+            if (error) {
+                res.status(500).json({
+                    error: error
+                });
+            }
+            else {
+                tweetsCache = tweets;
+                res.json(tweets);
+            }
+        });
+    }
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Listening on port 3000, not much has changed but they live on the water');
 });
